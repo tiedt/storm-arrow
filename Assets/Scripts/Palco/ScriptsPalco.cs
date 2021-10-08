d2d0305dad61777c610bef22592b68d01db3aa7b
lkcoelho <38146467+lkcoelho@users.noreply.github.com>
2021-10-08 20:40:45 -0300

    Captura dos botões

--- Assets/Scripts/Palco/ScriptsPalco.cs
+++ Assets/Scripts/Palco/ScriptsPalco.cs
@@ -2,10 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScriptsPalco : MonoBehaviour{
-    
-    void Start(){
-        
-    }    
+public class ScriptsPalco : MonoBehaviour
+{
+
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown((KeyCode.LeftArrow)))
+        {
+            print("Esquerda");
+        }
+        else if (Input.GetKeyDown((KeyCode.RightArrow)))
+        {
+            print("Direita");
+        }
+        else if (Input.GetKeyDown((KeyCode.DownArrow)))
+        {
+            print("Baixo");
+        }
+        else if (Input.GetKeyDown((KeyCode.UpArrow)))
+        {
+            print("Cima");
+        }
+    }
 
 }
