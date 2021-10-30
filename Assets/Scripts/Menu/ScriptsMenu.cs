90fb7e01af3187b0a94f9bb494e658aaee8fcb4f
William Mello <62627712+wilmello@users.noreply.github.com>
2021-10-29 21:22:19 -0300

    Ao carregar a cena do palco trazer o volume setado
    Ao carregar a cena do palco trazer o volume setado

--- Assets/Scripts/Menu/ScriptsMenu.cs
+++ Assets/Scripts/Menu/ScriptsMenu.cs
@@ -8,12 +8,27 @@ public class ScriptsMenu : MonoBehaviour{
     public Text lbperfil;
     public Text lbpontuacao;
     public GameObject MenuOpcoes;
+    public Slider volumeMusica;
 
     public void Start() {
+        //volumeMusica = GameObject.FindGameObjectWithTag("SliderMenuOpcoes").GetComponent<Slider>();
+        volumeMusica = FindObject<GameObject>("SliderVolumePrincipal").GetComponent<Slider>();
+        //volumeMusica = GameObject.Find("SliderVolumePrincipal").GetComponent<Slider>();
         if (PerfilLogado.Instance.conectado) {
             lbperfil.text = PerfilLogado.Instance.nome;
             lbpontuacao.text = "P "+ Convert.ToString(PerfilLogado.Instance.pontuacao_Total);
+            int index = PerfilLogado.Instance.Configuracoes.IndexOf(new PerfilConfiguracoes(){ config = "VolumePrincipal", idPerfil = PerfilLogado.Instance.id });
+            volumeMusica.value = Convert.ToInt32(PerfilLogado.Instance.Configuracoes[index].valor);
         }
+    }
+
+    public GameObject FindObject<T>(string name) where T : UnityEngine.Object {
+        T[] objects = Resources.FindObjectsOfTypeAll<T>() as T[];
+        foreach(T obj in objects){
+            if(obj.name.Equals(name, StringComparison.OrdinalIgnoreCase))
+                return obj as GameObject;
+        }
+        return null;
     }
 
     public void MudarCenaJogar(){
@@ -30,18 +45,16 @@ public class ScriptsMenu : MonoBehaviour{
     public void AbrirOpcoes() {
         if(MenuOpcoes != null) { 
             MenuOpcoes.SetActive(true);
-            Slider sliderMenuOpcoes = GameObject.FindGameObjectWithTag("SliderMenuOpcoes").GetComponent<Slider>();
-            sliderMenuOpcoes.value = Musica.PercentualVolume;
+            volumeMusica.value = Musica.PercentualVolume;
         }
     }
 
     public void ConfirmarOpcoes() {
-        Slider sliderMenuOpcoes = GameObject.FindGameObjectWithTag("SliderMenuOpcoes").GetComponent<Slider>();
-        Musica.DefinirVolumeMusica(null, sliderMenuOpcoes.value);
+        Musica.DefinirVolumeMusica(null, volumeMusica.value);
         PerfilConfiguracoes configuracao = new PerfilConfiguracoes(){
             idPerfil = PerfilLogado.Instance.id,
             config = "VolumePrincipal",
-            valor = Convert.ToString(sliderMenuOpcoes.value)
+            valor = Convert.ToString(volumeMusica.value)
         };
         PerfilLogado.Instance.AtualizaConfiguracaoPerfil(configuracao);
         StartCoroutine(ServicosHttp<PerfilConfiguracoes>.AtualizaConteudoServidor($@"{Enderecos.PerfilConfiguracoes}", configuracao));
--- Assets/Scripts/PerfilConfiguracoes.cs
+++ Assets/Scripts/PerfilConfiguracoes.cs
@@ -8,5 +8,17 @@ public class PerfilConfiguracoes{
     public string valor;
 
     public PerfilConfiguracoes() { }
-    
+
+    public override bool Equals(object obj) {
+        if(obj == null)
+            return false;
+        if(obj == this)
+            return true;
+        if(obj.GetType() != this.GetType())
+            return false;
+        PerfilConfiguracoes other = (PerfilConfiguracoes) obj;
+        return other.idPerfil == other.idPerfil
+            && other.config.Equals(this.config, System.StringComparison.OrdinalIgnoreCase);
+    }
+
 }
